8e3cf5447a042658b82dc0651f92c1752e2bb114
Nechaev Vladimir Alekseevich <111578816+vanechaev@users.noreply.github.com>
2024-03-26 21:57:29 +0300

    Update cicd.tf

--- terraform/cicd.tf
+++ terraform/cicd.tf
@@ -1,6 +1,6 @@
 resource "null_resource" "gitlab_agent" {
   provisioner "local-exec" {
-    command = "helm repo add gitlab https://charts.gitlab.io && helm repo update && helm upgrade --install gitlab-agent gitlab/gitlab-agent --namespace gitlab-agent --create-namespace --set image.tag=v1.1.1 --set config.token=glpat-sUzANzG7jHEyB--3V9gM --set config.kasAddress=wss://kas.gitlab.com"
+    command = "helm repo add gitlab https://charts.gitlab.io && helm repo update && helm upgrade --install gitlab-agent gitlab/gitlab-agent --namespace gitlab-agent-gitlab-agent --create-namespace --set image.tag=v16.11.0-rc1 --set config.token=glagent-RzVLcJfZ7w14Za57LTp2inUJ3AyDSjyyTUU2FPop2YspMvskzg --set config.kasAddress=wss://kas.gitlab.com"
   }
   depends_on = [
     null_resource.gitlab_agent
